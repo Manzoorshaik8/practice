b90eed44b43eb71d0bd2a5a82ee71dedb79d51c8
shaik <shaik@MANZOORSHAIK>
2025-11-12 17:06:23 +0530

    second

---
+++ Javadebug/src/com/example/javadebug/Javadebugging.java
@@ -0,0 +1,18 @@
+package com.example.javadebug;
+
+public class Javadebugging {
+
+public static void main(String[]args)
+{
+	int a=10;
+	int b=20;
+	int c=10;
+	int sum=addnumbers(a,b,c);
+	System.out.println("sum ="+sum);
+}
+static int addnumbers(int x,int y,int z)
+{
+	int result =x+y+z;
+	return result;
+}
+}
