8018c40d50612d8ec2b6c8dcf21ca7bcc01a5630
Manzoorshaik8 <shaik.m@zigmaneural.com>
2025-11-12 14:59:58 +0530

    helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,9 @@
+public class helloworld
+  {
+    public static void main (String[]args)
+    {
+      String message ="Hello";
+      message =message+"World";
+      System.out.println(message);
+    }
+  }
