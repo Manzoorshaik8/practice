99c70c89543790fe61a1fdd95f061a41cc42dff8
Manzoorshaik8 <shaik.m@zigmaneural.com>
2025-11-12 17:07:56 +0530

    Javadebugging.java

--- Javadebug/src/com/example/javadebug/Javadebugging.java
+++ Javadebug/src/com/example/javadebug/Javadebugging.java
@@ -7,12 +7,14 @@ public static void main(String[]args)
 	int a=10;
 	int b=20;
 	int c=10;
-	int sum=addnumbers(a,b,c);
+	int d=5;
+	int sum=addnumbers(a,b,c,d);
 	System.out.println("sum ="+sum);
 }
-static int addnumbers(int x,int y,int z)
+static int addnumbers(int x,int y,int z,int o)
 {
-	int result =x+y+z;
+	int result =x+y+z+o;
 	return result;
 }
 }
+
